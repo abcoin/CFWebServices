2c434ad0436d92d047681ae8a5fae4b3b8fcbc19
Bruce-Zhou <zhoulf.wallst@gmail.com>
2014-07-04 10:30:59 -0400

    insert row

---
+++ insert email.sql
@@ -0,0 +1,1 @@
+INSERT INTO `abe`.`EmailOutgoing` (`requestId`, `priorityId`, `statusId`, `fromAddress`, `toAdresses`, `mailSubject`, `body`, `requestDateTime`, `sendDateTime`, `comment`) VALUES (NULL, NULL, '0', 'support@icoinhost.com', 'destination@example.com', 'title', 'body', NULL, NULL, NULL);
