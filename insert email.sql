53733507c0f1e149b5e56190b8186eb3a2e0bdf3
abcoin <assetbackedcoin@gmail.com>
2014-07-13 00:40:25 -0400

    Update insert email.sql

--- insert email.sql
+++ insert email.sql
@@ -1,1 +1,16 @@
 INSERT INTO `abe`.`EmailOutgoing` (`requestId`, `priorityId`, `statusId`, `fromAddress`, `toAdresses`, `mailSubject`, `body`, `requestDateTime`, `sendDateTime`, `comment`) VALUES (NULL, NULL, '0', 'support@icoinhost.com', 'destination@example.com', 'title', 'body', NULL, NULL, NULL);
+
+
+INSERT INTO `btcft`.`EmailOutgoing` (`requestId`, `priorityId`, `statusId`, `fromAddress`, `toAdresses`, `mailSubject`, `body`, `requestDateTime`, `sendDateTime`, `comment`) VALUES (NULL, NULL, '0', 'support@icoinhost.com', 'bitcoincds@gmail.com', 'title', 
+'marketcaller\,
+
+Before you can start you using Bitcoin Free Trade, you must confirm your
+email address.
+
+Please confirm your email address by clicking on the link below:
+
+https://btcft.com/uservalidationbyemail/confirm?u=537&c=66f5f5bafa76a3a6ad905efac87eaeed
+
+If you can not click on the link, copy and paste it to your browser manually.Bitcoin Free Trade
+https://btcft.com/
+', NULL, NULL, NULL);
